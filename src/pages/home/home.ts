43d07486e0e9f13718d9d33ccf3a305c76e282ae
haiquangtran <hai.quang.tran@gmail.com>
2017-05-21 12:44:47 +1200

    added navigation on home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -44,10 +44,11 @@ export class HomePage {
     newPageAlert.present();
   }
 
+  // Needs index parameter for deleting the page in future
   public openPage(page: GoalItem, index: number) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.navCtrl.setRoot(page.goal, { index: index });
+    this.navCtrl.push(page.goal, { index: index });
   }
 
 }
