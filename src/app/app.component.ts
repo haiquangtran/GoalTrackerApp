02b29ab62435625fd9e7686e2c79d2c761cea391
haiquangtran <hai.quang.tran@gmail.com>
2017-05-21 21:30:08 +1200

    changed colours

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
+      this.statusBar.backgroundColorByHexString('#000013');
       this.splashScreen.hide();
     });
   }
