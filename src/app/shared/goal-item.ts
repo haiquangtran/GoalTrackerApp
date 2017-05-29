82b64a71c0e9d6528b9d66e6013b562e66d6f20c
haiquangtran <hai.quang.tran@gmail.com>
2017-05-29 22:44:33 +1200

    added layout for goal page

--- src/app/shared/goal-item.ts
+++ src/app/shared/goal-item.ts
@@ -3,10 +3,12 @@ export class GoalItem {
     public descriptionPlan: string;
     public goal: any;
     public subGoals: any[] = [];
+    public createdDate: Date;
 
     constructor(title?: string, goal?: any, descriptionPlan?: string) {
         this.title = title;
         this.goal = goal;
         this.descriptionPlan = descriptionPlan;
+        this.createdDate = new Date();
     }
 }
